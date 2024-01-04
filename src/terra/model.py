47ed13b2d9d551fdd0280ce25399c25a84b08177
Timo Wendner <timo.wendner@gmx.net>
2024-01-04 21:26:59 +0100

    custom loss function

--- src/terra/model.py
+++ src/terra/model.py
@@ -6,18 +6,17 @@ class RNN(nn.Module):
     def __init__(self, config: dict):
         super(RNN, self).__init__()
 
-        bidirectional = config['bidirectional']
         hidden_size = config['hidden size']
-        input_size = 5
+        input_size = 26
         self.rnn = nn.LSTM(
             input_size,
             hidden_size,
-            bidirectional=bidirectional,
             batch_first=True
         )
         self.dropout = nn.Dropout(config['Dropout'])
-        hidden_size *= 2 if bidirectional else 1
-        self.linear = nn.Linear(hidden_size, 5)
+        self.linear = nn.Linear(hidden_size, 26)
+        self.octave = nn.Softmax(dim=2)
+        self.key = nn.Softmax(dim=2)
 
     def forward(self, x, lengths):
         x = pack_padded_sequence(
@@ -27,4 +26,6 @@ class RNN(nn.Module):
         x, _ = pad_packed_sequence(x, batch_first=True)
         x = self.dropout(x)
         x = self.linear(x)
+        x[5:15] = self.octave(x[5:15])
+        x[15:] = self.key(x[15:])
         return x
--- src/terra/utils.py
+++ src/terra/utils.py
@@ -11,15 +11,16 @@ def open_midi_file(path: str) -> list[tuple[float]]:
         path (str): path to the midi file
 
     Returns:
-        list[tuple[float]]: tuple of 
+        list[tuple[float]]: list of tuples of 
             - float: note staring point
             - float: note duration
             - float: note difference to the last starting-point
             - float: note velocity
-            - float: note key
+            - 8 floats: one-hot encoding of key
+            - 8 floats: one-hot encoding of octave
     """
     midi_file = MidiFile(path)
-    result = []
+    result = [(0,)*26]
     open_notes = {}
     current = 0
     last = 0
@@ -31,8 +32,15 @@ def open_midi_file(path: str) -> list[tuple[float]]:
             last = current
         elif msg.type == 'note_off' and (pitch := msg.note) in open_notes:
             start, diff, velocity = open_notes[pitch]
+
+            speed = 50_000
+            start, diff = start / speed, diff / speed
+            octave, key = divmod(pitch, 12)
+            octave, key = torch.eye(10)[octave], torch.eye(12)[key]
             del open_notes[pitch]
-            result.append((start, current - start, diff, velocity, pitch))
+            result.append(
+                (start, current/speed - start, diff, velocity, *key, *octave)
+            )
     return sorted(result)
 
 
@@ -40,5 +48,10 @@ class CombinedLoss(nn.Module):
     """Loss function for the Torch network."""
 
     def forward(self, predictions: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-        mse = torch.mean(torch.abs(predictions - targets))
-        return mse
+        cross_entropy = nn.CrossEntropyLoss()
+        mse = nn.MSELoss()
+
+        error = cross_entropy(predictions[:, 15:, :], targets[:, 15:, :])
+        error += cross_entropy(predictions[:, 5:15, :], targets[:, 5:15, :])
+        error += mse(predictions[:, :5, :], targets[:, :5, :])
+        return error
